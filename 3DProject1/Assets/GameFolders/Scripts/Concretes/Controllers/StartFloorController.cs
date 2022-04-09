316e9dfb115b5ce3e9b910989b78846b335691d3
Berk KILIÇ <berk.kilic0638@gmail.com>
2022-04-09 16:08:05 +0300

    StartfloorController
    StartFloorController start floor prefab'ine eklendi

---
+++ 3DProject1/Assets/GameFolders/Scripts/Concretes/Controllers/StartFloorController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project1.Contollers
+{
+    public class StartFloorController : MonoBehaviour
+    {
+        private void OnCollisionExit(Collision other)
+        {
+            PlayerController player = other.collider.GetComponent<PlayerController>();
+
+            if (player !=null)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+
+
+}
